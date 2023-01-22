d524bc9cd44ad22f4e62888d2205e77d9f6465ba
Anton <russtone@Antons-MacBook-Pro.local>
2023-01-22 17:39:15 +0000

    nvim: Add default options to keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,39 +1,41 @@
+local opts = { noremap = true, silent = true }
+
 -- Reload config.
-vim.keymap.set("n", "<leader>r", ":source $MYVIMRC<CR>")
+vim.keymap.set("n", "<leader>r", ":source $MYVIMRC<CR>", opts)
 
 -- Write file.
-vim.keymap.set("n", "<leader>w", ":w!<CR>")
+vim.keymap.set("n", "<leader>w", ":w!<CR>", opts)
 
 -- Quit the current window.
-vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>", opts)
 
 -- Clear last search highlighting.
-vim.keymap.set("n", "<leader><leader>", ":nohlsearch<CR>")
+vim.keymap.set("n", "<leader><leader>", ":nohlsearch<CR>", opts)
 
 -- Close all other windows except the current.
-vim.keymap.set("n", "<leader>o", ":only<CR>")
+vim.keymap.set("n", "<leader>o", ":only<CR>", opts)
 
 -- Select pasted text.
-vim.keymap.set("n", "gv", "`[v`]")
+vim.keymap.set("n", "gv", "`[v`]", opts)
 
 -- RSI (Readline Style Insertion)
 
 -- Go to the start of the line.
-vim.keymap.set("i", "<C-a>", "<C-o>^")
-vim.keymap.set("c", "<C-a>", "<C-o>0")
+vim.keymap.set("i", "<C-a>", "<C-o>^", opts)
+vim.keymap.set("c", "<C-a>", "<C-o>0", opts)
 
 -- Go to the end of the line.
-vim.keymap.set("i", "<C-e>", "<C-o>$")
+vim.keymap.set("i", "<C-e>", "<C-o>$", opts)
 
 -- Go forward.
-vim.keymap.set("i", "<C-f>", "<Right>")
-vim.keymap.set("c", "<C-f>", "<Right>")
+vim.keymap.set("i", "<C-f>", "<Right>", opts)
+vim.keymap.set("c", "<C-f>", "<Right>", opts)
 
 -- Go back.
-vim.keymap.set("i", "<C-b>", "<Left>")
+vim.keymap.set("i", "<C-b>", "<Left>", opts)
 
 -- Paste.
-vim.keymap.set("i", "<C-y>", "<C-o>p")
+vim.keymap.set("i", "<C-y>", "<C-o>p", opts)
 
 -- Del.
-vim.keymap.set("i", "<C-d>", "<Delete>")
+vim.keymap.set("i", "<C-d>", "<Delete>", opts)
