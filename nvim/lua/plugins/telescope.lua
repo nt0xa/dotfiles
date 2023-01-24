6f5db31886069f882de1381525936043696d769f
Anton <russtone@Antons-MacBook-Pro.local>
2023-01-24 00:03:19 +0000

    nvim: telescope keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,7 +13,8 @@ return {
       defaults = {
         mappings = {
           i = {
-            ["<esc>"] = actions.close
+            ["<esc>"] = actions.close,
+            ["<C-u>"] = false,
           },
         },
       },
@@ -23,7 +24,6 @@ return {
     vim.keymap.set("n", "<leader>f", builtin.find_files, {})
     vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>b", builtin.buffers, {})
-    vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
 
   end
 }
