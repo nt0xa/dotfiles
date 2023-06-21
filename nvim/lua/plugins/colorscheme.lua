bc470cf06279641a8a2656cde6fbac4d4ee6bf43
anton.prokhorov <anton.prokhorov@bytedance.com>
2023-06-21 10:18:02 +0100

    nvim: Change colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
   {
-    "arcticicestudio/nord-vim",
+    "shaunsingh/nord.nvim",
     lazy = false,
     priority = 1000,
     config = function()
+      vim.g.nord_italic = false
+      vim.g.nord_bold = false
+
       vim.cmd([[colorscheme nord]])
     end,
   }
