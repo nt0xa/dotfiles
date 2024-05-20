e80df8f806c1d2f3ec582c601dfc5eb06a8e10e2
anton.prokhorov <anton.prokhorov@bytedance.com>
2024-05-20 10:25:40 +0100

    nvim: telescope show all diagnostics

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -87,6 +87,7 @@ return {
     vim.keymap.set("n", "<leader>b", function() builtin.buffers(themes.get_dropdown({})) end, {})
     vim.keymap.set("n", "<leader>s", function() builtin.lsp_document_symbols(themes.get_dropdown({})) end, {})
     vim.keymap.set("n", "<leader>a", function() builtin.lsp_dynamic_workspace_symbols(themes.get_dropdown({})) end, {})
+    vim.keymap.set("n", "<leader>e", function() builtin.diagnostics(themes.get_dropdown({})) end, {})
     vim.keymap.set("n", "<leader>d",  function() telescope.extensions.file_browser.file_browser(themes.get_dropdown({})) end)
   end
 }
