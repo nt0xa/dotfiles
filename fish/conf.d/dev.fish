c3f8d9b9771083526102cf05ce349f32884c6e5d
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2026-01-14 19:09:52 +0000

    Expose 8000 port by default, added de command

--- fish/conf.d/dev.fish
+++ fish/conf.d/dev.fish
@@ -11,22 +11,38 @@ end
 # Run command in dev container (attach if running, start if not)
 function __dev_run_in_container
   set -l container_name (__dev_container_name)
-  set -l command $argv
   set -l container_home /home/linuxbrew
 
-  # Attach to running container or start fresh
+  # Separate env args from command
+  set -l env_args
+  set -l command
+
+  while test (count $argv) -gt 0
+    if test "$argv[1]" = "-e"
+      set -a env_args -e $argv[2]
+      set -e argv[1..2]
+    else
+      set command $argv
+      break
+    end
+  end
+
   if docker container inspect -f "{{.State.Running}}" $container_name &>/dev/null
-    docker exec -it $container_name $command
+    docker exec -it $env_args $container_name $command
   else
     docker run -it --rm \
       --name $container_name \
       -w /code \
       -v $PWD:/code \
       -v $XDG_CONFIG_HOME/fish/config.fish:{$container_home}/.config/fish/config.fish:ro \
-      -v $XDG_CONFIG_HOME/nvim:{$container_home}/.config/nvim:ro \
+      -v $XDG_CONFIG_HOME/nvim/init.lua:{$container_home}/.config/nvim/init.lua:ro \
+      -v $XDG_CONFIG_HOME/nvim/luasnippets:{$container_home}/.config/nvim/luasnippets:ro \
       -v $XDG_CONFIG_HOME/starship.toml:{$container_home}/.config/starship.toml:ro \
       -v {$DEV_CONTAINER_IMAGE}_data:{$container_home}/.local \
       -v {$DEV_CONTAINER_IMAGE}_cache:{$container_home}/.cache \
+      -v {$DEV_CONTAINER_IMAGE}_github:{$container_home}/.config/github-copilot \
+      -p 8000 \
+      $env_args \
       $DEV_CONTAINER_IMAGE $command
   end
 end
@@ -36,5 +52,9 @@ function dv --description "Run nvim in dev Docker container for the current dire
 end
 
 function ds --description "Run shell in dev Docker container for the current directory"
-  __dev_run_in_container fish
+  __dev_run_in_container $argv fish
+end
+
+function de --description "Exec command in dev Docker container for the current directory"
+  __dev_run_in_container $argv
 end
