8c74b5b3f4a0e0e9e5b87b20ee3219b27f3c6b48
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2026-01-17 18:50:54 +0000

    fish: simplify dev.fish

--- fish/conf.d/dev.fish
+++ fish/conf.d/dev.fish
@@ -14,12 +14,15 @@ function __dev_run_in_container
   set -l container_home /home/linuxbrew
 
   # Separate env args from command
-  set -l env_args
+  set -l flags
   set -l command
 
   while test (count $argv) -gt 0
     if test "$argv[1]" = "-e"
-      set -a env_args -e $argv[2]
+      set -a flags -e $argv[2]
+      set -e argv[1..2]
+    else if test "$argv[1]" = "-p"
+      set -a flags -p $argv[2]
       set -e argv[1..2]
     else
       set command $argv
@@ -29,13 +32,13 @@ function __dev_run_in_container
 
   # Support password managers SSH_AUTH_SOCK.
   if set -q SSH_AUTH_SOCK
-    set -a volume_args -v /run/host-services/ssh-auth.sock:/var/run/ssh-auth.sock
-    set -a env_args -e SSH_AUTH_SOCK=/var/run/ssh-auth.sock
+    set -a flags -v /run/host-services/ssh-auth.sock:/var/run/ssh-auth.sock
+    set -a flags -e SSH_AUTH_SOCK=/var/run/ssh-auth.sock
   end
 
   # Pass variables from .env files.
   if test -e $PWD/.env
-    set -a env_args --env-file .env
+    set -a flags --env-file .env
   end
 
   if docker container inspect -f "{{.State.Running}}" $container_name &>/dev/null
@@ -52,9 +55,7 @@ function __dev_run_in_container
       -v {$DEV_CONTAINER_IMAGE}_data:{$container_home}/.local \
       -v {$DEV_CONTAINER_IMAGE}_cache:{$container_home}/.cache \
       -v {$DEV_CONTAINER_IMAGE}_github:{$container_home}/.config/github-copilot \
-      $volume_args \
-      -p 8000 \
-      $env_args \
+      $flags \
       $DEV_CONTAINER_IMAGE $command
   end
 end
