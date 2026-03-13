0e3983cc0620c005f53aed51a8e7699e4f391802
Anton Prokhorov <anton.prokhorov@databricks.com>
2026-03-13 10:38:46 +0000

    fish: removed docker socket from dev container

--- fish/conf.d/dev.fish
+++ fish/conf.d/dev.fish
@@ -41,9 +41,6 @@ function __dev_run_in_container
     set -a envs -e SSH_AUTH_SOCK=/var/run/ssh-auth.sock
   end
 
-  # Add docker socket
-  set -a vols -v /var/run/docker.sock:/var/run/docker.sock
-
   # Pass variables from .env files.
   if test -e $PWD/.env
     set -a envs --env-file .env
